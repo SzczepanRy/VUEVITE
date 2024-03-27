b5849669507aa1942e330d1368562c0fd9730837
unknown <szep.rydzewski@gmail.com>
2024-03-27 02:39:03 +0100

    first commit

---
+++ l1/src/App1.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="main">
+        <MyButton2 :prop1="bg" :prop2="cl"></MyButton2>
+    </div>
+</template>
+
+<script>
+import MyButton2 from "./components/MyButton2.vue";
+export default {
+    data() {
+        return {
+            message: "Hello World",
+            bg: "red",
+            cl: "blue",
+        };
+    },
+    components: { MyButton2 },
+};
+</script>
+
+<style>
+#main {
+    font-size: 18px;
+    font-family: "Roboto", sans-serif;
+    color: blue;
+}
+</style>
